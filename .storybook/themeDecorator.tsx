145dfaadc64202d1092f45fba4b4bb8cd566e65c
BuildTools <luca.goslar@icloud.com>
2020-03-27 18:30:30 +0100

    storybook i18n integration

--- .storybook/themeDecorator.tsx
+++ .storybook/themeDecorator.tsx
@@ -4,11 +4,16 @@ import { ThemeProvider } from "styled-components";
 import defaultTheme from "../src/constants/theme/defaultTheme";
 import GlobalStyle from "../src/constants/globals";
 
+import { I18nextProvider } from "react-i18next";
+import i18n from "../src/constants/lang/i18n";
+
 const ThemeDecorator = storyFn => (
-  <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-    {storyFn()}
-  </ThemeProvider>
+  <I18nextProvider i18n={i18n}>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      {storyFn()}
+    </ThemeProvider>
+  </I18nextProvider>
 );
 
 export default ThemeDecorator;

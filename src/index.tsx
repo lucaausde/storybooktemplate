68506823fc20707a9bad35defd8f31b36f526524
BuildTools <luca.goslar@icloud.com>
2020-04-24 23:06:07 +0200

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,6 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./constants/lang/i18n";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();

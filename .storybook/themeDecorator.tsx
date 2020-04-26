5d0c64d5bc00a27d9659876e97bc061173abceba
BuildTools <luca.goslar@icloud.com>
2020-04-26 15:27:24 +0200

    bug fixes

--- .storybook/themeDecorator.tsx
+++ .storybook/themeDecorator.tsx
@@ -8,12 +8,12 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../src/constants/lang/i18n";
 
 const ThemeDecorator = storyFn => (
-  <I18nextProvider i18n={i18n}>
-    <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={defaultTheme}>
+    <I18nextProvider i18n={i18n}>
       <GlobalStyle />
       {storyFn()}
-    </ThemeProvider>
-  </I18nextProvider>
+    </I18nextProvider>
+  </ThemeProvider>
 );
 
 export default ThemeDecorator;
--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `yarn storybook`
+
+Run storybook.<br />
+Open [http://localhost:3333](http://localhost:3333) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
 ### `yarn start`
 
 Runs the app in the development mode.<br />
